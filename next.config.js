aead2f6d81cac5d4d60a1b28fba807f7b4d4f928
akimov99900 <89273259+akimov99900@users.noreply.github.com>
2025-11-22 13:31:27 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,16 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        // 👇 ВСТАВЬ СЮДА СВОЮ ДЛИННУЮ ССЫЛКУ ИЗ ЗЕЛЕНОГО ПОЛЯ 👇
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019aab19-2a94-dade-0b1f-e494516f13d5', 
+        permanent: false,
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
